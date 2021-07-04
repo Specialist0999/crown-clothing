bede1953d8ccbbb4f8f2dd65ebcc052483fab638
Paawan <paawanbahl1999@gmail.com>
2021-07-04 20:36:15 +0530

    finished signin handlesubmit method

--- src/pages/signin-and-signup/signin-and-signup.component.js
+++ src/pages/signin-and-signup/signin-and-signup.component.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Signin from '../../components/signin/signin.component';
+import SignUp from '../../components/signup/signup.component';
 import './signin-and-signup.styles.scss';
 
 const SignInAndSignUpPage = () => {
 	return (
 		<div className='sign-in-and-sign-up'>
 		<Signin />
+		<SignUp />
 		</div>
 		)
 }
